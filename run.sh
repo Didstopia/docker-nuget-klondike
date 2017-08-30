699718fc3de1fa5f8fda9f3ce7ff8d21227610aa
Pauli Jokela <pauli.jokela@didstopia.com>
2017-08-30 16:37:22 +0300

    Trying to fix compatibility with Docker Cloud

--- run.sh
+++ run.sh
@@ -3,17 +3,20 @@
 set -e
 set -o pipefail
 
-SOURCE="/app"
-TARGET="/data"
-SETTINGS_FILE="Settings.config"
+#SOURCE="/app"
+#TARGET="/data"
+#SETTINGS_FILE="Settings.config"
 
-if [ ! -f "$TARGET/$SETTINGS_FILE" ]; then
-	echo "Fresh install detected, applying default configuration.."
-	cp -fr $SOURCE/$SETTINGS_FILE $TARGET/$SETTINGS_FILE
-fi
+#if [ ! -f "$TARGET/$SETTINGS_FILE" ]; then
+#	echo "Fresh install detected, applying default configuration.."
+#	cp -f $SOURCE/$SETTINGS_FILE $TARGET/$SETTINGS_FILE
+#	rm -f $SOURCE/$SETTINGS_FILE
+#fi
 
-ln -sf $TARGET/$SETTINGS_FILE $SOURCE/$SETTINGS_FILE
+#ln -sf $TARGET/$SETTINGS_FILE $SOURCE/$SETTINGS_FILE
 
-nginx
+#nginx
 
-exec mono /app/bin/Klondike.SelfHost.exe --interactive --port=8080
+exec mono /app/bin/Klondike.SelfHost.exe --interactive --port=8080 &
+
+nginx -g 'daemon off;'
